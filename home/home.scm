ac9f4e217bcecfe5a73f058d7bca2d2076f93aca
Paul <paul@teulu.org>
2022-01-12 22:16:07 +0000

    Added .gitignore  - added .inputrc section to home.scm  - formatting in config.scm

--- home/home.scm
+++ home/home.scm
@@ -2,12 +2,33 @@
 	     (gnu home services)
 	     (gnu home services shells)
 	     (gnu services)
-	     (gnu packages admin)
+	     (gnu packages)
 	     (guix gexp))
 
+(define %inputrc
+  (plain-file
+   "inputrc"
+   (string-append
+    "set input-meta on\n"
+    "set convert-meta off\n"
+    "set output-meta on\n\n"
+    "set editing-mode emacs\n\n"
+
+    "$if mode=emacs\n"
+    "# Allow the use of the home/end keys\n"
+    "\"\\e[1~\": beginning-of-line\n"
+    "\"\\e[4~\": end-of-line\n\n"
+    "# Map 'page up' and 'page down' to search history based on current cmdline\n"
+    "\"\\e[5~\": history-search-backward\n"
+    "\"\\e[6~\": history-search-forward\n"
+    "$endif\n")))
+
+(define %home-apps
+  (list "htop"))
 
 (home-environment
- (packages (list htop))
+ (packages (map specification->package+output
+		%home-apps))
  (services
   (list
    (service home-bash-service-type
@@ -15,5 +36,11 @@
 	     (guix-defaults? #t)
 	     (environment-variables
 	      '(("XDG_CACHE_HOME" . "~/.cache")
-		("HISTFILE" . "$XDG_CACHE_HOME/.bash_history"))))))))
+		("INPUTRC" . "~/.inputrc")
+		("HISTFILE" . "$XDG_CACHE_HOME/.bash_history")))))
+
+   (simple-service 'inputrc-config
+		   home-files-service-type
+		   (list `("inputrc"
+			   ,%inputrc))))))
 
